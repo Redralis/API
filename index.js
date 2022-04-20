aaf63d0bcad279041ce0a04938afff3e959767ce
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-04-20 16:55:54 +0200

    Added yml for Heroku integration and added a couple of api functions.

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
 const express = require('express');
 const app = express()
 const port = 3000;
+const bodyParser = require('body-parser');
 
-app.get('/', )
+app.use(bodyParser.json());
+
+let database = [];
+let id = 0;
+
+app.all('*', (req, res, next) => {
+  const method = req.method
+  console.log(`Methode ${method} aangeroepen`)
+  next()
+});
+
+app.post('/api/movie', (req, res) => {
+  let movie = req.body;
+  console.log(movie);
+  id++;
+  movie = {
+    id,
+    ...movie,
+  };
+  database.push(movie);
+  console.log(database)
+  res.status(201).json({
+    status: 201,
+    result: movie,
+  });
+});
+
+app.get('/api/movie', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: database,
+  });
+});
+
+app.get('/api/movie/:movieId', (req, res) => {
+  const movieId = req.params.movieId;
+  let movie = database.filter((item) => (item.id == movieId));
+  if (movie.length > 0) {
+    res.status(200).json({
+      status: 200,
+      result: movie,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `Movie with ID ${movieId} not found`,
+    });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: 'Hello World!',
+  });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    result: 'End-point not found',
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
