bdae849421b4b13696487f36c49f311db9ea8156
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-04-29 12:43:21 +0200

    Upgraded to mysql connection pool

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 const bodyParser = require('body-parser');
 const userRouter = require('./src/routes/user.routes');
 const { env } = require('process');
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,5 @@
-const assert = require('assert');
-let database = [];
-let id = 0;
+const dbconnection = require('../../database/dbconnection')
+const assert = require('assert')
 
 let controller = {
     standardResponse: (req, res) => {
@@ -58,11 +57,30 @@ let controller = {
       }
     },
 
-    getAllUsers: (req, res) => {
-      res.status(200).json({
-          status: 200,
-          result: database,
-      });
+    getAllUsers: (req, res, next) => {
+      dbconnection.getConnection(function (err, connection) {
+          if (err) throw err // not connected!
+
+          // Use the connection
+          connection.query(
+              'SELECT id, name FROM meal;',
+              function (error, results, fields) {
+                // When done with the connection, release it.
+                connection.release()
+                // Handle error after the release.
+                if (error) throw error
+                // Don't use the connection here, it has been returned to the pool.
+                console.log('#results = ', results.length)
+                res.status(200).json({
+                  status: 200,
+                  results: results,
+                })
+                // pool.end((err) => {
+                //     console.log('pool was closed.')
+                // })
+              }
+          )
+      })
     },
 
     getPersonalProfile: (req, res) => {
