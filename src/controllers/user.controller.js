7e2f5c140f49961aad5438ae876727b845d55753
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-04-29 14:06:45 +0200

    Rewrote code for adding a user and for getting all users to use mysql.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,53 +31,45 @@ let controller = {
 
     addUser: (req, res, next) => {
       let user = req.body;
-      const emailAdress = user.emailAdress;
-      user = {
-        id,
-        ...user,
-      };
-      //If the new user's email is unique, the user will be pushed to the database. Otherwise, an error will be returned.
-      if (!database.some(e => e.emailAdress === emailAdress)) {
-        id++;
-        while (database.some(e => e.id === id)) {
-          id++;
-        }
-        database.push(user);
-        console.log(database)
-        res.status(201).json({
-          status: 201,
-          result: user,
-        });
-      } else {
-        const error = {
-          status: 422,
-          result: `Email address ${emailAdress} is already registered.`,
-        }
-        next(error);
-      }
+      dbconnection.getConnection(function (err, connection, next) {
+        if (err) throw err
+        connection.query(
+          "INSERT INTO user (firstName, lastName, street, city, password, emailAdress) VALUES ('" + user.firstName + "', '" + 
+          user.lastName + "', '" + user.street + "', '" + user.city + "', '" + user.password + "', '" + user.emailAdress + "');",
+          function (error) {
+            if (error) {
+              if(error.errno==1062){   
+                res.status(422).json({
+                  status: 422,
+                  result: 'Email address is already registered',
+                });
+              } else {
+                throw error;
+              }
+            } else {
+            connection.release()
+            res.status(201).json({
+              status: 201,
+              result: user,
+            });
+            }
+          })
+      })
     },
 
-    getAllUsers: (req, res, next) => {
+    getAllUsers: (req, res) => {
       dbconnection.getConnection(function (err, connection) {
-          if (err) throw err // not connected!
-
-          // Use the connection
+          if (err) throw err
           connection.query(
-              'SELECT id, name FROM meal;',
-              function (error, results, fields) {
-                // When done with the connection, release it.
+              'SELECT * FROM user;',
+              function (error, results) {
                 connection.release()
-                // Handle error after the release.
                 if (error) throw error
-                // Don't use the connection here, it has been returned to the pool.
-                console.log('#results = ', results.length)
+                console.log('Amount of users = ', results.length)
                 res.status(200).json({
                   status: 200,
                   results: results,
                 })
-                // pool.end((err) => {
-                //     console.log('pool was closed.')
-                // })
               }
           )
       })
